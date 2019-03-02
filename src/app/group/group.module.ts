9308bb14cc9adb26fc46bb2873d859087bf25308
Jorge Dacosta <jorgemsdc@mail.com>
2019-03-02 21:04:12 +0000

    fix (#1): ❌remove Core module

--- src/app/group/group.module.ts
+++ src/app/group/group.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupComponent } from './group/group.component';
-import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 import { GroupWikiComponent } from './group-wiki/group-wiki.component';
 import { GroupStreamComponent } from './group-stream/group-stream.component';
@@ -29,7 +28,6 @@ import { GroupRoutingModule } from './group-routing.module';
   ],
   imports: [
     CommonModule,
-    CoreModule,
     SharedModule,
     GroupRoutingModule
   ],
